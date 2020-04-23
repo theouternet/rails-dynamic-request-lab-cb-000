e10f71c2e03475b9031fadc2291486fc01c1ec04
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-23 01:53:41 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,14 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+    def show
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
+  
 end
