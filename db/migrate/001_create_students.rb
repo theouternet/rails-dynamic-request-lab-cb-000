4c4578086913a54b39de8da905b4c4f9133dbbc7
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-23 01:38:18 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,8 +1,8 @@
-class CreatePosts < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
-      t.string :title 
-      t.text :description
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
